0733720cded821f005521835f9f7fcd644f5e9c5
Nick Van den Broeck <nick.van.den.broeck666@gmail.com>
2018-04-30 15:24:27 +0200

    This code causes errors

--- src/Grenade/Examples/NN/Network.hs
+++ src/Grenade/Examples/NN/Network.hs
@@ -13,8 +13,10 @@ import Grenade.Examples.NN.Param
 
 import Control.Monad.Random.Class
 
-type NN
-     = Network '[ Reshape, FullyConnected 784 30, Tanh, FullyConnected 30 10, Logit] '[ Image, 'D1 784, 'D1 30, 'D1 30, 'D1 10, Label]
+--type NN
+--     = Network '[ Reshape, FullyConnected 784 30, Tanh, FullyConnected 30 10, Logit] '[ Image, 'D1 784, 'D1 30, 'D1 30, 'D1 10, Label]
+
+type NN = Network '[ Reshape, FullyConnected 784 10] '[ Image, 'D1 784, Label]
 
 randomNetworkM :: MonadRandom m => m NN
 randomNetworkM = randomNetwork
